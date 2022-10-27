6b4289fbefb4874b458462ce3021d956a676be0b
smb6088 <smb6088@mavs.uta.edu>
2022-10-27 17:47:22 -0500

    P08 bonus

--- P08/bonus/reverse.cpp
+++ P08/bonus/reverse.cpp
@@ -7,12 +7,9 @@ int main(int argc, char* argv[])
   for (int i = 1; i < argc; ++i)
   	{
 
-    	std::string si(argv[i]);
+    	std::string si{argv[i]};
     	std::reverse(si.begin(), si.end());
     	std::cout << si << "\n";
 	}
-    
-
-
 	return 0;
 }
