a6fe23c7f19cf9ad22abc7c569f98946718b80e6
smb6088 <smb6088@mavs.uta.edu>
2022-10-26 20:50:15 -0500

    P08 extreme bonus

--- P08/extreme_bonus/sorted.cpp
+++ P08/extreme_bonus/sorted.cpp
@@ -5,21 +5,18 @@ int main()
 {
 	std::string s;
 	std::vector<std::string> v;
-	while(std::cin >> s )
+	while(true)
 	{
+		std::cin >> s;
 		if(std::cin.eof())
 		{
 			break;
 			
 		}
 		v.push_back(s);
-		//v.push_back("A");
-		//v.push_back("ZBF");
-		//v.push_back("BBB");
-		//v.push_back("algorithm");
 	}
 	std::sort(v.begin(),v.end());
-
+	std::cout << "Sorted:" << "\n";
 	for(auto c : v)
 	{
 		std::cout << c << "\n";
